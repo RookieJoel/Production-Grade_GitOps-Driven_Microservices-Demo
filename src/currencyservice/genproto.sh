664b272b98780ef9b7ee902707352039ee097fdc
Suttanop Joel Chanah <jojackchanah@gmail.com>
2026-06-03 20:03:03 +0700

    Update genproto.sh

--- src/currencyservice/genproto.sh
+++ src/currencyservice/genproto.sh
@@ -17,7 +17,7 @@
 # [START gke_currencyservice_genproto]
 
 # protos are loaded dynamically for node, simply copies over the proto.
-mkdir -p proto
+mkdir -p proto 
 cp -r ../../protos/* ./proto
 
 # [END gke_currencyservice_genproto]
